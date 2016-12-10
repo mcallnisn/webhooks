351322db248fc232688f2b02bc6b2bc93e155f3b
Michael Wilson <michaelwilson@Michaels-MacBook-Pro.local>
2016-12-09 22:20:00 -0700

    Initial Commit

---
+++ src/main/java/org/of/denver/webhooks/beans/CloudBeansJob.java
@@ -0,0 +1,10 @@
+package org.of.denver.webhooks.beans;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by michaelwilson on 12/9/16.
+ */
+@XmlRootElement
+public class CloudBeansJob {
+}
---
+++ src/test/java/org/of/denver/webhooks/controllers/PullRequestHooksControllerTest.java
@@ -0,0 +1,17 @@
+package org.of.denver.webhooks.controllers;
+
+import org.junit.Test;
+
+/**
+ * Created by michaelwilson on 12/9/16.
+ */
+public class PullRequestHooksControllerTest {
+
+    private final PullRequestHooksController pullRequestHooksController = new PullRequestHooksController();
+
+    @Test
+    public void test() throws Exception{
+
+    }git
+
+}
